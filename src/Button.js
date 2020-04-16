eb29a5d6ea1b075840c8f6911d33ec8166c387db
Kritika Verma <kverma@netspend.com>
2020-04-16 22:56:15 +0530

    new commit now

--- src/Button.js
+++ src/Button.js
@@ -4,7 +4,7 @@ export class Button extends Component {
     render() {
         return (
             <div>
-              <button onClick ={this.props.click}>{this.props.value}</button>  
+              <button onClick ={this.props.click}>Hello{this.props.value}</button>  
             </div>
         )
     }
